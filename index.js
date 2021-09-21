53e534e1610a3d8e5dde6a4869a9ab03e7a8edbc
Henrique Souza <henrique.asouza@outlook.com>
2021-09-21 18:37:16 -0300

    feat: add ask page and route

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ app.get("/", (req, res) => {
     res.render("index")
 })
 
+app.get("/perguntar", (req, res) => {
+    res.render("ask")
+})
+
 app.listen(8080, () => {
     console.log("App rodando.")
 })
