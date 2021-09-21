990583714b83adf264ee7c5860df47760e39d1b8
Henrique Souza <henrique.asouza@outlook.com>
2021-09-21 12:36:46 -0300

    feat: add index js

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express")
+const app = express()
+
+// EJS as View Engine
+app.set('view engine', 'ejs')
+
+app.get("/", (req, res) => {
+    res.render("index")
+})
+
+app.listen(8080, () => {
+    console.log("App rodando.")
+})
