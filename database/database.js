35c8991bd1f50d27613efe954a8fecfcfd92bbef
Henrique Souza <henrique.asouza@outlook.com>
2021-09-22 16:29:37 -0300

    refactor: add to database env variables

--- database/database.js
+++ database/database.js
@@ -1,8 +1,10 @@
 require('dotenv').config()
 const Sequelize = require("sequelize")
+const env = process.env.NODE_ENV || 'development';
+const config = require(__dirname + '/../config/config.json')[env];
 
-const connection = new Sequelize('guiaperguntasdb', process.env.DB_USER, process.env.DB_PASS, {
-    host: process.env.host,
+const connection = new Sequelize(config.database, process.env.DB_USER, process.env.DB_PASS, {
+    host: process.env.DB_HOST,
     dialect: 'mysql'
 })
 
