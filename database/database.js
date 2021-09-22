b7d53f4ae59b55a6d37fd5513c7b3ea82043faba
Henrique Souza <henrique.asouza@outlook.com>
2021-09-22 17:59:42 -0300

    refactor: database with user and pass comming from config file

--- database/database.js
+++ database/database.js
@@ -1,11 +1,11 @@
-require('dotenv').config()
-const Sequelize = require("sequelize")
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+require("dotenv").config();
+const Sequelize = require("sequelize");
+const env = process.env.NODE_ENV || "development";
+const config = require(__dirname + "/../config/config.json")[env];
 
-const connection = new Sequelize(config.database, process.env.DB_USER, process.env.DB_PASS, {
+const connection = new Sequelize(config.database, config.username, config.password, {
     host: config.host,
-    dialect: 'mysql'
-})
+    dialect: "mysql",
+});
 
-module.exports = connection
+module.exports = connection;
