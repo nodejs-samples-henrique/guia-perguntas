adb5442c2faa08d4023e4629f483fec406c7e0db
Henrique Souza <henrique.asouza@outlook.com>
2021-09-22 22:19:13 -0300

    feat: answer migrations

---
+++ models/answers.js
@@ -0,0 +1,24 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Answers extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  Answers.init({
+    body: DataTypes.TEXT,
+    questionId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Answers',
+  });
+  return Answers;
+};
